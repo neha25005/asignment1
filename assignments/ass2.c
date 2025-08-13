344068976282293be5af636d051f3e21185b4b88
Neha Bhakta <neha.bhakta.2005@gmail.com>
2025-08-13 08:42:30 +0530

    Add files via upload
    assignment 1 has been uploaded

---
+++ assignments/ass2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+    {
+        float a , b,c ;
+        printf("enter two numbers\n");
+        scanf("%f %f", &a, &b);
+         c= a+b;
+         printf("the required sum: %f" , c);
+        return 0;
+    }
---
+++ assignments/ass5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("enter a character:\n");
+    scanf("%c", &ch);
+    printf(" the ASCII value ofn %c is %d \n", ch,(int)ch);
+    return 0;
+}
