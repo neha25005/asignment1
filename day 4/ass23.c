a03ce804e563f34e1e36153697312f09d80215c8
Neha Bhakta <neha.bhakta.2005@gmail.com>
2025-09-01 15:33:11 +0530

    Add files via upload

---
+++ day 4/ass23.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter an alphabet:\n");
+    scanf(" %c", &ch);   // space before %c to avoid newline issues
+
+    if (ch == 'a' || ch == 'A') {
+        printf("%c is a vowel.\n", ch);
+    }
+    else if (ch == 'e' || ch == 'E') {
+        printf("%c is a vowel.\n", ch);
+    }
+    else if (ch == 'i' || ch == 'I') {
+        printf("%c is a vowel.\n", ch);
+    }
+    else if (ch == 'o' || ch == 'O') {
+        printf("%c is a vowel.\n", ch);
+    }
+    else if (ch == 'u' || ch == 'U') {
+        printf("%c is a vowel.\n", ch);
+    }
+    else {
+        printf("%c is a consonant.\n", ch);
+    }
+
+    return 0;
+}
