0dc954b9e0cb239911e51df01a9d7cf4f8a2d650
Neha Bhakta <neha.bhakta.2005@gmail.com>
2025-09-01 15:36:01 +0530

    Update ass14.c

--- day 3/ass14.c
+++ day 3/ass14.c
@@ -1,20 +1,14 @@
-#include <stdio.h> 
-#include <math.h>  
+#include <stdio.h>
+#include <math.h>   // for floor() and ceil()
 
 int main() {
-    double positive_number;
-    double negative_number ;
-    printf("enter a positive integer:\n");
-    scanf("%f",&positive_number);
-    printf("enter a negative integer:\n");
-    scanf("%f",&negative_number);
-    printf("For the positive number %.2f:\n", positive_number);
-    printf("Floor: %f\n", floor(positive_number));
-    printf("Ceiling: %f\n", ceil(positive_number));
-    printf("\n"); 
-    printf("For the negative number %.2f:\n", negative_number);
-    printf("Floor: %f\n", floor(negative_number));
-    printf("Ceiling: %f\n", ceil(negative_number));
+    double num;
+
+    printf("Enter a number (positive or negative): ");
+    scanf("%lf", &num);
+
+    printf("Floor value of %.2lf = %.2lf\n", num, floor(num));
+    printf("Ceiling value of %.2lf = %.2lf\n", num, ceil(num));
 
     return 0;
 }
